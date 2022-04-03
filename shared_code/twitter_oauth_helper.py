ae0efd1f6afaa04072186d21e70127b3deab767e
Yusuke Shinmi <Yusuke.Shinmi@gmail.com>
2022-04-03 16:52:44 +0900

    Add test for aouth helper.

--- shared_code/twitter_oauth_helper.py
+++ shared_code/twitter_oauth_helper.py
@@ -4,9 +4,9 @@ from requests_oauthlib import OAuth1Session
 from os import environ
 
 def create_session() -> OAuth1Session:
-  token = environ['TWITTER_ACCESS_TOKEN']
-  token_secret = environ['TWITTER_TOKEN_SECRET']
-  consumer_key = environ['TWITTER_API_KEY']
-  consumer_secret = environ['TWITTER_API_SECRET_KEY']
+    token = environ['TWITTER_ACCESS_TOKEN']
+    token_secret = environ['TWITTER_TOKEN_SECRET']
+    consumer_key = environ['TWITTER_API_KEY']
+    consumer_secret = environ['TWITTER_API_SECRET_KEY']
 
-  return OAuth1Session(consumer_key, consumer_secret, token, token_secret)
+    return OAuth1Session(consumer_key, consumer_secret, token, token_secret)
---
+++ tests/test_twitter_oauth_helper.py
@@ -0,0 +1,25 @@
+"""The Unit Test for twitter_aouth_helper.py
+"""
+
+import unittest
+from unittest.mock import Mock
+from unittest.mock import patch
+
+import azure.functions as func
+from shared_code import twitter_oauth_helper
+
+class TestTwitterAOuth(unittest.TestCase):
+    """Test class for Param class to use twitter destory API."""
+
+    @patch.dict('os.environ', 
+        {'TWITTER_ACCESS_TOKEN': 'access-token',
+        'TWITTER_TOKEN_SECRET': 'token-secret', 
+        'TWITTER_API_KEY': 'api-key', 
+        'TWITTER_API_SECRET_KEY': 'api-secret-key'})
+    @patch('shared_code.twitter_oauth_helper.OAuth1Session')
+    def test_create_session(self, mock):
+        twitter_oauth_helper.create_session()
+        mock.assert_called_once_with('api-key', 'api-secret-key', 'access-token', 'token-secret')
+
+if __name__ == '__main__':
+    unittest.main()
