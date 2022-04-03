6c2419127a3de06dc4841ddfd044439282f13244
Yusuke Shinmi <Yusuke.Shinmi@gmail.com>
2022-04-03 15:38:10 +0900

    Add test for update API.

--- shared_code/twitter_update_helper.py
+++ shared_code/twitter_update_helper.py
@@ -1,29 +1,13 @@
 # <project_root>/shared_code/twitter_update_helper.py
 
 import json
-from shared_code import twitter_oauth_helper
+from shared_code import twitter_proxy
 
-class Param:
-  def __init__(self, status: str):
-    self._param = {
-      'status': status
-    }
+class Param(twitter_proxy.ParamInterface):
+    def __init__(self, status: str):
+        self._param = {
+            'status': status
+        }
 
-  def convert_to_query(self) -> str:
-    return { k: self._param[k] for k in self._param if None != self._param[k] }
-
-def request(param: Param) -> str:
-  endpoint_url = 'https://api.twitter.com/1.1/statuses/update.json'
-
-  client = twitter_oauth_helper.create_session()
-
-  params = param.convert_to_query()
-
-  res = client.post(endpoint_url, params=params)
-
-  if res.status_code == 200:
-    res = json.loads(res.text)
-  else:
-    raise RuntimeError('Network Error. status code: {res.status_code}')
-
-  return res
+    def get_endpoint_url(self) -> str:
+        return 'https://api.twitter.com/1.1/statuses/update.json'
---
+++ tests/test_twitter_update_helper.py
@@ -0,0 +1,22 @@
+"""The Unit Test for twitter_update_helper.py
+"""
+
+import unittest
+from unittest.mock import patch
+
+import azure.functions as func
+from shared_code import twitter_update_helper
+
+class TestTwitterUpdateParam(unittest.TestCase):
+    """Test class for Param class to use twitter update API."""
+
+    def test_constructor(self):
+        param = twitter_update_helper.Param('text')
+        self.assertEqual(param._param['status'], 'text')
+
+    def test_endpoint_url(self):
+        param = twitter_update_helper.Param('text')
+        self.assertEqual(param.get_endpoint_url(), 'https://api.twitter.com/1.1/statuses/update.json')
+
+if __name__ == '__main__':
+    unittest.main()
