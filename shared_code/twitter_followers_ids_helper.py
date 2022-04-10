5f67d62dbd7abb45b91fe7c9013a8b302e939fea
Yusuke Shinmi <Yusuke.Shinmi@gmail.com>
2022-04-10 19:55:05 +0900

    Add unit test for twitter followeers ids API.

---
+++ shared_code/twitter_followers_ids_helper.py
@@ -0,0 +1,54 @@
+# <project_root>/shared_code/twitter_followers_ids_helper.py
+
+from os import environ
+from shared_code import twitter_proxy
+
+class Param(twitter_proxy.ParamInterface):
+    def __init__(self):
+        self._param = {
+            'user_id': environ['TWITTER_USER_ID'],
+            'screen_name': None,
+            'cursor': None,
+            'stringify_ids': None,
+            'count': 5000
+        }
+
+    def set_user_id(self, id: str) -> None:
+        self._param['user_id'] = id
+
+    def get_user_id(self) -> None:
+        return self._param['user_id']
+
+    def set_screen_name(self, name: str) -> None:
+        self._param['screen_name'] = name
+
+    def get_screen_name(self) -> str:
+        return self._param['screen_name']
+
+    def set_cursor(self, cursor: str) -> None:
+        self._param['cursor'] = cursor
+
+    def get_cursor(self) -> str:
+        return self._param['cursor']
+
+    def set_stringify_ids(self, stringify_ids: str) -> None:
+        self._param['stringify_ids'] = stringify_ids
+
+    def get_stringify_ids(self) -> str:
+        return self._param['stringify_ids']
+
+    def set_count(self, cnt: int) -> None:
+        up_to_count = 5000
+
+        if cnt <= 0:
+            self._param['count'] = 1
+        elif cnt <= up_to_count:
+            self._param['count'] = cnt
+        else:
+            self._param['count'] = up_to_count
+
+    def get_count(self) -> int:
+        return self._param['count']
+
+    def get_endpoint_url(self) -> str:
+        return 'https://api.twitter.com/1.1/followers/ids.json'
---
+++ tests/test_twitter_followers_ids_helper.py
@@ -0,0 +1,86 @@
+# 
+import unittest
+from unittest.mock import patch
+
+from shared_code import twitter_followers_ids_helper
+
+
+class TestTwitterFollowersIdsHelper(unittest.TestCase):
+
+    @patch.dict('os.environ', {'TWITTER_USER_ID': 'test-user-id'})
+    def setUp(self) -> None:
+        self._param = twitter_followers_ids_helper.Param()
+
+    def tearDown(self) -> None:
+        self._param = None
+
+    def test_init_error(self) -> None:
+        try:
+            twitter_followers_ids_helper.Param()
+        except Exception as e:
+            self.assertTrue(True)
+
+    def test_user_id1(self) -> None:
+        self.assertEqual(self._param.get_user_id(), 'test-user-id')
+
+    def test_user_id2(self) -> None:
+        self._param.set_user_id('user2')
+        self.assertEqual(self._param.get_user_id(), 'user2')
+
+    def test_screen_name1(self) -> None:
+        self.assertIsNone(self._param.get_screen_name())
+
+    def test_screen_name2(self) -> None:
+        self._param.set_screen_name('screen')
+        self.assertEqual(self._param.get_screen_name(), 'screen')
+
+    def test_cursor1(self) -> None:
+        self.assertIsNone(self._param.get_cursor())
+
+    def test_cursor2(self) -> None:
+        self._param.set_cursor('cursor')
+        self.assertEqual(self._param.get_cursor(), 'cursor')
+
+    def test_stringify_ids1(self) -> None:
+        self.assertIsNone(self._param.get_stringify_ids())
+
+    def test_stringify_ids2(self) -> None:
+        self._param.set_stringify_ids('ids')
+        self.assertEqual(self._param.get_stringify_ids(), 'ids')
+
+    def test_count1(self) -> None:
+        self.assertEqual(self._param.get_count(), 5000)
+
+    def test_count2(self) -> None:
+        self._param.set_count(-1)
+        self.assertEqual(self._param.get_count(), 1)
+
+    def test_count3(self) -> None:
+        self._param.set_count(0)
+        self.assertEqual(self._param.get_count(), 1)
+
+    def test_count4(self) -> None:
+        self._param.set_count(1)
+        self.assertEqual(self._param.get_count(), 1)
+
+    def test_count5(self) -> None:
+        self._param.set_count(2)
+        self.assertEqual(self._param.get_count(), 2)
+
+    def test_count6(self) -> None:
+        self._param.set_count(4999)
+        self.assertEqual(self._param.get_count(), 4999)
+
+    def test_count7(self) -> None:
+        self._param.set_count(5000)
+        self.assertEqual(self._param.get_count(), 5000)
+
+    def test_count8(self) -> None:
+        self._param.set_count(5001)
+        self.assertEqual(self._param.get_count(), 5000)
+
+    def test_endpoint_url(self) -> None:
+        self.assertEqual(self._param.get_endpoint_url(), 'https://api.twitter.com/1.1/followers/ids.json')
+
+if __name__ == '__main__':
+    unittest.main()
